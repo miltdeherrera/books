3faa620b3fe524b62d9a178db29981a5f193a585
Milt Deherrera <miltdeherrera@gmail.com>
2021-12-09 09:44:13 -0800

    Refactor into classes, certain features remain to be reimplemented.

--- books.js
+++ books.js
@@ -1,83 +1,311 @@
-//  initializing the array that holds the Books
-let myLibrary = [];
+// REFACTORING
 
-//  the library DOM object
-const libraryDOM = document.querySelector(".library");
+class LibraryViewer {
+    constructor(libraryList) {
+        this.fillLibraryDOM(libraryList.getLibraryContents());
+    };
 
-//  initializing the class of Book objects
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${read ? 'have read' : 'not read yet'}`;
+    _libraryDOM = document.querySelector(".library");
+
+
+    currentDOM = function () {
+        return this._libraryDOM;
+    }
+}
+
+LibraryViewer.prototype.fillLibraryDOM = function (libraryArray) {
+    for (let book of libraryArray) {
+        const cardToAdd = book.getCardNode();
+        cardToAdd.dataset.libraryIndex = book.serial;
+        this.currentDOM().appendChild(cardToAdd);
     };
 }
 
-// function adds all books passed to it into the library.
-function addBookToLibrary(...args) {
+LibraryViewer.prototype.emptyLibrary = function () {
+    //
+    while (this._libraryDOM.firstChild) {
+        this._libraryDOM.removeChild(this._libraryDOM.firstChild);
+    }
+};
+
+LibraryViewer.prototype.refreshLibrary = function () {
+    this.emptyLibrary();
+    this.fillLibraryDOM();
+}
+
+
+
+class Library {
+    constructor(...args) {
+        this._bookList = [];
+        this._libraryIndex = 0;
+        this._libraryDOM = document.querySelector(".library");
+    }
+
+    getLibraryContents = function () {
+        return this._bookList;
+    }
+}
+
+LibraryViewer.prototype.emptyLibraryDOM = function () {
+    while (this.getLibraryDOM().firstChild) {
+        this.getLibraryDOM().removeChild(this.getLibraryDOM().firstChild);
+    }
+};
+
+Library.prototype.addBooksToLibrary = function (...args) {
     for (let book of args) {
-        myLibrary.push(book);
-    }
-}
+        book.serial = this._libraryIndex++;
+        const cardButtonRow = document.createElement('div');
+        cardButtonRow.className = "new-book-pane-button";
 
-Book.prototype.drawCard = function () {
-    let newCard = document.createElement("div");
+        // Adding the remove button
+        const removeButton = document.createElement('button');
+        removeButton.textContent = "Remove";
+        removeButton.addEventListener("click", () => {
+            this.removeFromLibrary(book.serial);
+            book.getCardNode().parentElement.removeChild(book.getCardNode());
+        });
+        cardButtonRow.appendChild(removeButton);
 
-    // go through each book element and add it as a paragraph to the card
-    const cardTextArray = [`Title: ${this.title}`, `Author: ${this.author}`, `# of pages: ${this.pages}`, `${this.read ? 'have read' : 'not read yet'}`];
-    for (let i of cardTextArray) {
-        let newLine = document.createElement("p");
-        newLine.textContent = i;
-        newCard.appendChild(newLine);
-    }
-
-    // remove button code
-    // create button
-    const cardButtonRow = document.createElement('div');
-    cardButtonRow.className = "new-book-pane-button";
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = "Remove";
-    removeButton.addEventListener("click", () => removeCard(newCard));
-    cardButtonRow.appendChild(removeButton);
-
-    // remove button code ends here
-
-    // read status toggle button - toggles the read status for the card and the book
-    const NewBookPaneReadToggle = document.createElement("button");
-    NewBookPaneReadToggle.textContent = "Change read status";
-    NewBookPaneReadToggle.addEventListener("click", () => {
-        // change read status in library
-        // takes data key
-        // accesses myLibrary entry
-        // runs toggleReadStatus
-        myLibrary[NewBookPaneReadToggle.
-            parentElement.
-            parentElement.dataset.libraryIndex].toggleReadStatus();
-        // change read status text on card
-        let card = document.getElementsByClassName("card");
-        card[NewBookPaneReadToggle.
-            parentElement.
-            parentElement.dataset.libraryIndex].children[3].textContent = myLibrary[NewBookPaneReadToggle.
+        // Adding the "read yet" toggle
+        const NewBookPaneReadToggle = document.createElement("button");
+        NewBookPaneReadToggle.textContent = "Change read status";
+        NewBookPaneReadToggle.addEventListener("click", () => {
+            this._bookList[NewBookPaneReadToggle.
                 parentElement.
-                parentElement.dataset.libraryIndex].read ? 'have read' : 'not read yet';
+                parentElement.dataset.libraryIndex].toggleReadStatus();
+            NewBookPaneReadToggle.
+                parentElement.
+                parentElement = book.resetCardNode();
 
 
-    });
-
-    cardButtonRow.appendChild(NewBookPaneReadToggle);
-    newCard.appendChild(cardButtonRow);
 
 
-    newCard.className = "card";
-    return newCard;
+
+            // IMPLEMENT FUNCTIONALITY
+
+            // find element in book array with serial matching "NewBookPaneReadToggle.parentElement.parentElement.dataset.libraryIndex", then toggle its read status
+
+
+
+            // change read status in library
+            // takes data key
+            // accesses myLibrary entry
+            // runs toggleReadStatus
+
+
+
+            // this._bookList[NewBookPaneReadToggle.
+            //     parentElement.
+            //     parentElement.dataset.libraryIndex].toggleReadStatus();
+            // // change read status text on card
+            // this._bookList[NewBookPaneReadToggle.
+            //     parentElement.
+            //     parentElement.dataset.libraryIndex]._cardNode.textContent =
+            //     this._bookList[NewBookPaneReadToggle.
+            //         parentElement.
+            //         parentElement.dataset.libraryIndex].read ? "Read: true" : "Read: false";
+
+
+        });
+
+        cardButtonRow.appendChild(NewBookPaneReadToggle);
+        book.getCardNode().appendChild(cardButtonRow);
+
+        this._bookList.push(book);
+    }
+
 }
 
-Book.prototype.toggleReadStatus = function () {
-    this.read = !(this.read);
+Library.prototype.removeFromLibrary = function (serial) {
+    // if a book with that particular serial exists in the array, 
+    // remove that entry from the array
+    // array => newBook object => property serial
+    for (let i = 0; i < this._bookList.length; i++) {
+        if (this._bookList[i]._serial === serial) {
+            this._bookList.splice(serial, 1);
+            break;
+        }
+    }
+
 }
+
+
+
+class newBook {
+    constructor(title,
+        author,
+        pages,
+        read) {
+        // _info array consists of info objects pertaining to the book.
+        this._info = new Map([
+            ['title', title],
+            ['author', author],
+            ['pages', pages],
+            ['read', read],
+        ]);
+        this._serial = -1;
+        this._cardNode = new Card(this._info);
+    }
+
+    getCardNode = function () {
+        return this._cardNode;
+    }
+
+    // need to test
+    toggleReadStatus = function () {
+        this._info.set('read', !(this._info.get('read')));
+    }
+
+    set serial(value) {
+        this._serial = value;
+    }
+
+    get serial() {
+        return this._serial;
+    }
+
+}
+
+newBook.prototype.getInfo = function () {
+    return this._info;
+}
+
+newBook.prototype.resetCardNode = function () {
+    this._cardNode = new Card(this._info);
+}
+
+
+
+class Card {
+    constructor(infoMap) {
+        // creates a blank card element, which we will append to the DOM
+        let newCard = document.createElement("div");
+
+        // go through each book element and add it as a paragraph to the card
+        for (let infoPair of infoMap) {
+            let newLine = document.createElement("p");
+            // text of new paragraph element is:
+            //      The title of the property in proper case
+            //      a colon
+            //      the rest of the title
+            newLine.textContent = `${infoPair[0].charAt(0).toUpperCase()}${infoPair[0].slice(1)}: ${infoPair[1]}`;
+            newCard.appendChild(newLine);
+        }
+
+        newCard.className = "card";
+        return newCard;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  initializing the array that holds the Books
+// let myLibrary = [];
+
+// //  the library DOM object
+// const libraryDOM = document.querySelector(".library");
+
+// //  initializing the class of Book objects
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = function () {
+//         return `${title} by ${author}, ${pages} pages, ${read ? 'have read' : 'not read yet'} `;
+//     };
+// }
+
+// // function adds all books passed to it into the library.
+// function addBookToLibrary(...args) {
+//     for (let book of args) {
+//         myLibrary.push(book);
+//     }
+// }
+
+// Book.prototype.drawCard = function () {
+//     let newCard = document.createElement("div");
+
+//     // go through each book element and add it as a paragraph to the card
+//     const cardTextArray = [`Title: ${this.title} `, `Author: ${this.author} `, `# of pages: ${this.pages} `, `${this.read ? 'have read' : 'not read yet'} `];
+//     for (let i of cardTextArray) {
+//         let newLine = document.createElement("p");
+//         newLine.textContent = i;
+//         newCard.appendChild(newLine);
+//     }
+
+//     // remove button code
+//     // create button
+//     const cardButtonRow = document.createElement('div');
+//     cardButtonRow.className = "new-book-pane-button";
+
+//     const removeButton = document.createElement('button');
+//     removeButton.textContent = "Remove";
+//     removeButton.addEventListener("click", () => {
+//         myLibrary.splice(card.dataset.libraryIndex);
+//         libraryDOM.removeChild(card);
+//     });
+//     cardButtonRow.appendChild(removeButton);
+
+// remove button code ends here
+
+// read status toggle button - toggles the read status for the card and the book
+// const NewBookPaneReadToggle = document.createElement("button");
+// NewBookPaneReadToggle.textContent = "Change read status";
+// NewBookPaneReadToggle.addEventListener("click", () => {
+//     // change read status in library
+//     // takes data key
+//     // accesses myLibrary entry
+//     // runs toggleReadStatus
+//     myLibrary[NewBookPaneReadToggle.
+//         parentElement.
+//         parentElement.dataset.libraryIndex].toggleReadStatus();
+//     // change read status text on card
+//     let card = document.getElementsByClassName("card");
+//     card[NewBookPaneReadToggle.
+//         parentElement.
+//         parentElement.dataset.libraryIndex].children[3].textContent = myLibrary[NewBookPaneReadToggle.
+//             parentElement.
+//             parentElement.dataset.libraryIndex].read ? 'have read' : 'not read yet';
+
+
+// });
+
+// cardButtonRow.appendChild(NewBookPaneReadToggle);
+// newCard.appendChild(cardButtonRow);
+
+
+// newCard.className = "card";
+// return newCard;
+// }
+
+// Book.prototype.toggleReadStatus = function () {
+//     this.read = !(this.read);
+// }
 
 // find data value libraryIndex in card, delete it.
 // go to parent of card, delete card.
@@ -224,36 +452,31 @@ const createNewBookPane = function () {
 };
 
 
-
-
-
 // following code sets up the testing environment
 
 
 
-
-
-
-// initializes some books
-const the_stand = new Book(
+const testStand = new newBook(
     "The Stand",
     "Stephen King",
     1348,
     true);
 
-const la_confidential = new Book(
+const testLA = new newBook(
     "LA Confidential",
     "James Ellroy",
     510,
     true
 );
 
-const rigoberta = new Book(
+const testRig = new newBook(
     "I, Rigoberta Menchu",
     "Rigoberta Menchu",
     322,
     false
 );
 
-addBookToLibrary(the_stand, la_confidential, rigoberta);
-fillLibrary(myLibrary);
+const testLibrary = new Library();
+testLibrary.addBooksToLibrary(testStand, testLA, testRig);
+
+const libraryView = new LibraryViewer(testLibrary);
